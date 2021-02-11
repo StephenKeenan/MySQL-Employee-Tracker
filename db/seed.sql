ffc6454a6dea99f2595014991bfd61d507aea177
Stephen Keenan <stephenkeenan02@gmail.com>
2021-02-11 14:06:14 -0500

    seed work

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,10 @@
 USE employees;
 
 INSERT INTO department (name)
-VALUES('Legal')('Sales')
+VALUES ('Legal'),('Sales');
 
+INSERT INTO role (title, salary, department_id)
+VALUES("cashier", 1000, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Smith", 1, null);
