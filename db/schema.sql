5f240827671db0f5df11169604b0c4576a5f5637
Stephen Keenan <stephenkeenan02@gmail.com>
2021-02-10 21:23:56 -0500

    completed schema - works now

--- db/schema.sql
+++ db/schema.sql
@@ -4,30 +4,33 @@ CREATE DATABASE employees;
 USE employees;
 
 CREATE TABLE department (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL
-);
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+); 
 
 CREATE TABLE role (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) UNIQUE NOT NULL,
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL UNSIGNED NOT NULL,
-    department_id INT NOT NULL,
-        FOREIGN KEY (department_id)
-         REFERENCES department(id)
-         ON DELETE CASCADE
+    department_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) UNIQUE NOT NULL,
-    last_name VARCHAR(30) UNIQUE NOT NULL,
-    role_id INT NOT NULL,
-    FOREIGN KEY (role_id)
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id)
          REFERENCES role(id)
-         ON DELETE CASCADE
-    manager_id INT NULL,
-    FOREIGN KEY (manager_id)
+         ON DELETE CASCADE,
+    manager_id INT UNSIGNED NULL,
+    CONSTRAINT fk_employee FOREIGN KEY (manager_id)
          REFERENCES employee(id)
          ON DELETE CASCADE
-)
+);
