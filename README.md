f6cc7180a6a7fe6d9733a0e487259ac313d34e5b
Stephen Keenan <stephenkeenan02@gmail.com>
2021-02-10 20:39:15 -0500

    schema completion

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ Design the following database schema containing three tables:
   * **id** - INT PRIMARY KEY
   * **first_name** - VARCHAR(30) to hold employee first name
   * **last_name** - VARCHAR(30) to hold employee last name
-  * **role_id** - INT to hold reference to role employee has * MySQL foriegn key
+  * **role_id** - INT to hold reference to role employee has * MySQL foreign key
   * **manager_id** - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
   
 Build a command-line application that at a minimum allows the user to:
--- db/schema.sql
+++ db/schema.sql
@@ -11,5 +11,23 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
-    salary DECIMAL UNSIGNED NOT NULL
+    salary DECIMAL UNSIGNED NOT NULL,
+    department_id INT NOT NULL,
+        FOREIGN KEY (department_id)
+         REFERENCES department(id)
+         ON DELETE CASCADE
 );
+
+CREATE TABLE employee (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) UNIQUE NOT NULL,
+    last_name VARCHAR(30) UNIQUE NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id)
+         REFERENCES role(id)
+         ON DELETE CASCADE
+    manager_id INT NULL,
+    FOREIGN KEY (manager_id)
+         REFERENCES employee(id)
+         ON DELETE CASCADE
+)
